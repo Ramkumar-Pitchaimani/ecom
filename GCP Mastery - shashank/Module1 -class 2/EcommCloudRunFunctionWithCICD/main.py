ba2385797c415e30fbeacb1fe85442859f457961
Ramkumar <p.raamkumar@gmail.com>
2025-09-29 11:15:11 +0530

    v2

--- GCP Mastery - shashank/Module1 -class 2/EcommCloudRunFunctionWithCICD/main.py
+++ GCP Mastery - shashank/Module1 -class 2/EcommCloudRunFunctionWithCICD/main.py
@@ -22,7 +22,7 @@ def order_event(request):
     HTTP Cloud Run Function to handle 'order.created' events.
     Expects a rich JSON payload (see utils/order_utils.validate_payload).
     """
-    # test comment
+    # test 
     logger.info("Received %s %s", request.method, request.path)
     if request.method != "POST":
         logger.warning("Only POST allowed")
